46f87f7cc096a0c5bad81fc046a9969b5c6c8072
ccojocea <ccojocea@gmail.com>
2018-01-30 22:40:39 +0200

    Finish About implementation

--- app/src/main/java/com/example/android/ccojocea/javaquiz/AboutActivity.java
+++ app/src/main/java/com/example/android/ccojocea/javaquiz/AboutActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -15,7 +16,7 @@ import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity{
 
-    TextView tvClickable1, tvClickable2, tvClickable3;
+    TextView tvClickable1, tvClickable2, tvClickable3, tvClickableAuthorLicense, tvClickableFontLicense;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,19 +26,29 @@ public class AboutActivity extends AppCompatActivity{
         Spanned clickableLink1 = Html.fromHtml(getString(R.string.logo_cc_2_link));
         Spanned clickableLink2 = Html.fromHtml(getString(R.string.logo_cc_5_link));
         Spanned clickableLink3 = Html.fromHtml(getString(R.string.logo_cc_7_link));
+        Spanned clickableLink4 = Html.fromHtml(getString(R.string.about_author_5));
+        Spanned clickableLink5 = Html.fromHtml(getString(R.string.font_license_5_web));
 
         tvClickable1 = findViewById(R.id.about_clickable_link_1);
         tvClickable2 = findViewById(R.id.about_clickable_link_2);
         tvClickable3 = findViewById(R.id.about_clickable_link_3);
+        tvClickableAuthorLicense = findViewById(R.id.about_clickable_link_author_2);
+        tvClickableFontLicense = findViewById(R.id.about_font_clickable);
 
         tvClickable1.setText(clickableLink1);
         tvClickable2.setText(clickableLink2);
         tvClickable3.setText(clickableLink3);
-
-        tvClickable1.setTextColor(ContextCompat.getColor(this, R.color.colorSecondaryDark));
+        tvClickableFontLicense.setText(clickableLink5);
+        tvClickableAuthorLicense.setText(clickableLink4);
 
         tvClickable1.setMovementMethod(LinkMovementMethod.getInstance());
         tvClickable2.setMovementMethod(LinkMovementMethod.getInstance());
         tvClickable3.setMovementMethod(LinkMovementMethod.getInstance());
+        tvClickableAuthorLicense.setMovementMethod(LinkMovementMethod.getInstance());
+        tvClickableFontLicense.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public void goBack(View view){
+        this.onBackPressed();
     }
 }
